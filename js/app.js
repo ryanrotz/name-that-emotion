02eb8a4a213902e298d9fa20d47ac6561cd97a39
Ryan Rotz <ryanrotz@gmail.com>
2016-06-05 15:16:04 -0700

    Added html framework and listed the functions needed

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+
+// Create an object that includes the images and their associated feeling:
+
+Var levelOne = {
+  happy: url,
+  Sad: url,
+  Mad: url,
+  Nervous: url
+};
+
+
+var levelTwo {
+
+}
+
+
+// Function that pulls an image from an array and displays it in the "image box"
+
+
+// Function that pulls emotions from an array and puts them in button1, 2, 3, 4
+
+
+// Function that determines if the correct answer/button is pushed
+
+
+// Function that displays "Correct" popup or "Keep Trying" popup
+
+// Function that displays the completed emotion in the "Correct" popup
+
+// Function that determines whether to (from the popup window) go back to
+// the game board or to the "You completed Level X" page
+
+// Function that displays list of completed emotions in the "You completed Level X" page
+
+// Funtion that determines whether to display "Start" level" 2" "Start level 3" or "Get Trophy"
+
+
+
+
+
+// Javascript/html/css for two popups.
+// Javascript/html/css for "you completed level X" page
+// Javascript/html/css for Trophy page
+// Javascript/html/css for home page
