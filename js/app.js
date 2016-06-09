0a34ce399ac29016b0e830023f3aced51194c28d
Ryan Rotz <ryanrotz@gmail.com>
2016-06-09 09:26:43 -0700

    finished game with level one. Use for week 3 project presentation

--- js/app.js
+++ js/app.js
@@ -4,28 +4,6 @@ $(document).ready(function() {
   //   console.log();
   // })
 
-// Create an object that includes the images and their associated feeling:
-
-// var levelOne {
-//   hurt:
-//   guilty:
-//   tired:
-//   jealous:
-//   hopeful:
-//   bored:
-//   proud:
-//   sorry:
-//   embarrassed:
-// };
-
-// var levelTwoArray = [
-// {name: "shy", link: "https://atrts.files.wordpress.com/2010/06/shy.jpeg"},
-// {name: "afraid", link: "http://vaxtruth.org/wordpress/wp-content/uploads/2012/05/child-200x300.jpg"},
-// {name: "excited", link: "https://melissakircher.files.wordpress.com/2013/12/excited.jpeg"},
-// {name: "bored", link: "http://i.huffpost.com/gen/1867389/thumbs/h-KID-BORED-960x540.jpg"},
-// {name: "surprised", link: "https://s3.amazonaws.com/lifesite/shocked_face_child.jpg"},
-// ]
-
 
 var levelOneArray = [];
 var levelOneNewArray = [];
@@ -33,26 +11,9 @@ var correctSelection;
 var counter;
 
 
-// When the page is loaded the start button should be big and in the middle
-// of the screen. The header should be hidden.
-
-$('#startGame').click(function(e) {
-  e.preventDefault();
-  console.log('game started');
-  $('.intro').hide();
-  $('.board').show();
-  initialize();
-  reset();
-  play();
-});
-
-
-
 // Calls the initialize and reset functions when the page loads.
 // The reset button does the same thing.
 // The game is ready to play. The play must then click the start button
-
-
 function initialize () {
   levelOneArray = [
   {name: "shy", link: "https://atrts.files.wordpress.com/2010/06/shy.jpeg"},
@@ -87,11 +48,6 @@ function reset () {
 
 
 function play() {
-  // if(levelOneArray.length < 5) {
-  //   console.log('level one complete');
-  //   $('#levelOneComplete').modal();
-  // };
-
   // The first element is selected. The image appears and the name is stored in a variable "correctSelection"
   $('#imagebox').html("<img class='img-responsive' src="+levelOneNewArray[0].link+">");
     correctSelection = levelOneNewArray[0].name;
@@ -108,12 +64,20 @@ function play() {
 };
 
 
+// When the page is loaded the start button should be big and in the middle
+// of the screen. The header should be hidden.
+
+$('#startGame').click(function(e) {
+  e.preventDefault();
+  console.log('game started');
+  $('.intro').hide();
+  $('.board').show();
+  initialize();
+  reset();
+  play();
+});
+
 $('#resetbutton').click(function() {
-  // $('#imagebox').html('');
-  // $('#button1').html('');
-  // $('#button2').html('');
-  // $('#button3').html('');
-  // $('#button4').html('');
   initialize();
   reset();
 });
@@ -123,19 +87,16 @@ $('#start').click(function() {
   play();
 });
 
-
 // Tells us if the correct button is pushed
 $('#button1, #button2, #button3, #button4').click(function(){
   if($(this).text() === correctSelection) {
     console.log('correct!');
-    //e.preventDefault();
     $('#correctModal').modal();
-    // add points here???
+
   } else {
       // pop up "keepTryingModal" modal
       console.log('incorrect');
       $('#keepTryingModal').modal();
-        // reset(); // reset button --> might just want to put this in the modal instead
   }
 });
 
@@ -143,7 +104,7 @@ $('#button1, #button2, #button3, #button4').click(function(){
 // When pressed the modal hides, game is reset, and points are stored
 $('#correctNextButton').click(function(){
   levelOneArray.splice(0,1);
-  counter++;
+  counter = (counter + 10);
   $('#points').text('Points: '+counter);
 
   if(levelOneArray.length < 5) {
@@ -161,27 +122,24 @@ $('#keepTryingButton').click(function(){
   play();
 })
 
+
+$('#startLevelTwoButton').click(function() {
+  initialize();
+  reset();
+  play();
+})
+
 // $('button').on('click', function() {
 //     $(this).animate({backgroundColor: 'green'});
 //   });
 
 
 
-
-
-
-
-// loop 4 times
-//   $(div to attach img).append("<img src="+levelOne[random math].link+">")
-//   $(div to attach button).append("<button>"+levelOne[random math].name+"</button>")
-
 // DONE Function that pulls emotions from an array and puts them in button1, 2, 3, 4
-
 
 // DONE Function that determines if the correct answer/button is pushed
 
-
-// Function that displays "Correct" popup or "Keep Trying" popup
+// DONE Function that displays "Correct" popup or "Keep Trying" popup
 
 // Function that displays the completed emotion in the "Correct" popup
 
@@ -193,14 +151,10 @@ $('#keepTryingButton').click(function(){
 // Funtion that determines whether to display "Start" level" 2" "Start level 3" or "Get Trophy"
 
 
-
-
-
-// Javascript/html/css for two popups.
+// Javascript/html/css for two popups. DONE
+// Javascript/html/css for home page DONE
 // Javascript/html/css for "you completed level X" page
 // Javascript/html/css for Trophy page
-// Javascript/html/css for home page
-
 
 
 });  // end of document.ready function. Don't put any code after this.
@@ -209,75 +163,3 @@ $('#keepTryingButton').click(function(){
 
 
 
-
-//var levelOneObj = [];
-// var numberOfButtons = 4;
-
-
-
-// var levelTwo = [
-//   {name:frightened, link: ""},
-//   {name: suspicious, link: ""},
-//   disappointed:
-//   curious:
-//   horrified:
-//   relieved:
-//   arrogant:
-// ]
-
-// var leveThree = [
-//   disgusted:
-//   anxious:
-//   cautious:
-//   indifferent:
-//   innocent:
-//   interested:
-//   mischevious:
-//   perplexed:
-//   satisfied:
-//   shocked:
-// ]
-
-
-// Function that pulls a random image from an array and displays it in the "image box"
-// var randomN;
-
-// function randomNumber (array) {
-//   randomN = Math.floor(Math.random()*array.length);
-// };
-
-// randomNumber(levelOneArray);
-
-// for(i = 0; i < numberOfButtons; i++) {
-//   randomNumber(levelOneArray);
-//   levelOneObj[i] = levelOneArray[randomN];
-//   // storedLevelOneArray.push(levelOneArray.splice(randomN, 1)[0]);
-// };
-
-// randomNumber();
-// var levelOneObj2 = levelOneArray[randomN];
-// storedLevelOne.push(levelOne.splice(randomN, 1)[0]);
-
-// randomNumber();
-// var levelOneObj3 = levelOneArray[randomN];
-// storedLevelOne.push(levelOne.splice(randomN, 1)[0]);
-
-// randomNumber();
-// var levelOneObj4 = levelOneArray[randomN];
-// storedLevelOne.push(levelOne.splice(randomN, 1)[0]);
-
-// var levelOneImage = levelOne[Math.floor(Math.random()*levelOne.length)];
-// var levelOneImage2 = levelOne[Math.floor(Math.random()*levelOne.length)];
-// var levelOneImage3 = levelOne[Math.floor(Math.random()*levelOne.length)];
-// var levelOneImage4 = levelOne[Math.floor(Math.random()*levelOne.length)];
-
-
-//console.log(levelOneObj);
-
-// // mixes up the objects in the array into a new order
-// levelOneArray.sort(function() { return 0.5 - Math.random() });
-
-// // Push the first four objects of the mixed up array into a new array
-// for(i = 0; i < 4; i++) {
-//   levelOneNewArray.push(levelOneArray[i]);
-// }
